e14e3b7e177c8ee40ebd3af25666350d180f8e46
Linas Varabauskas <Linas.Varabauskas.3@gmail.com>
2026-05-03 22:24:39 +0100

    Accepting filename as command-line argument

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
-int main(void) {
-    printf("Hello, World!\n");
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Usage: %s <csv_filename>\n", argv[0]);
+        return 1; // Returns 1 if either no files or too many files found in program arguments
+    }
+
+    printf("Power Quality Waveform Analyser\n");
+    printf("Input file: %s\n", argv[1]);
+
     return 0;
 }
